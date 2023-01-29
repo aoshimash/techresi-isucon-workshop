0e43150bb2cdda94200e96c0fd60c56ed0fb9163
Tatsuaki Machida <machida@mi.t.u-tokyo.ac.jp>
2023-01-30 00:16:11 +0900

    until Using net/http to serve wiki pages

---
+++ ISUCON_Go勉強会/gowiki/wiki.go
@@ -0,0 +1,41 @@
+//go:build ignore
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "log"
+    "net/http"
+)
+
+type Page struct {
+    Title string
+    Body  []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func main() {
+    http.HandleFunc("/view/", viewHandler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
